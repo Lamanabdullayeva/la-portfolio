1f69e8a0bb8da495109eed9403c5b746630baf17
Laman Abdullayeva <lemon.abdulla@gmail.com>
2026-07-23 14:07:10 +0200

    instagram link fixed

--- src/app/core/constants/social-links.constants.ts
+++ src/app/core/constants/social-links.constants.ts
@@ -1,5 +1,5 @@
 export const SOCIAL_LINKS = {
   github: "https://github.com/Lamanabdullayeva",
   linkedin: "https://www.linkedin.com/in/laman-abdullayeva/",
-  instagram: "https://www.instagram.com/handmadechokers_la/",
+  instagram: "https://www.instagram.com/chokercult_/",
 } as const;
